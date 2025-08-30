a6723ef6f3a1dab6e3cb4bb269b2816b1921cd12
kctffs <155545578+kctffs@users.noreply.github.com>
2025-08-30 02:00:21 +0000

    imported summernote, enhanced admin panel

--- newsonthego/settings.py
+++ newsonthego/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'news-on-the-go-2.herokuapp.com',
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_summernote",
     "news",
 ]
 
