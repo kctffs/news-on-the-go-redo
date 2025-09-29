ec980afb742e60119873a46a4faa564f3fcb443b
kctffs <155545578+kctffs@users.noreply.github.com>
2025-09-29 01:27:32 +0000

    installed django forms, created commenting form

--- newsonthego/urls.py
+++ newsonthego/urls.py
@@ -20,8 +20,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("news.urls"), name="news-urls"),
     path('summernote/', include('django_summernote.urls')),
-    path("admin/", admin.site.urls),
 ]
