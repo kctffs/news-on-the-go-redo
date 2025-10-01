dad15afdb53b91a01c6b8431cd6825132305deeb
kctffs <155545578+kctffs@users.noreply.github.com>
2025-10-01 23:28:38 +0000

    forms testing with multple tests

---
+++ news/test_forms.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from .forms import CommentForm
+
+class TestCommentForm(TestCase):
+
+    def test_form_is_valid(self):
+        """
+        Test that the CommentForm is valid when a body is provided.
+        """
+        comment_form = CommentForm({'body': 'This post exceeds expectations'})
+        self.assertTrue(comment_form.is_valid(), msg="Form is invalid when it should be valid")
+
+    def test_form_is_invalid(self):
+        """
+        Test that the CommentForm is invalid when no body is provided.
+        """
+        comment_form = CommentForm({'body': ''})
+        self.assertFalse(comment_form.is_valid(), msg="Form is valid when it should be invalid")
+
+
+class TestCommentFormValidation(TestCase):
+    """
+    Test the CommentForm for different types of input to be sure
+    it validates correctly.
+    """
+
+    def test_form_valid_with_regular_text(self):
+        """
+        Form should be valid with normal text input.
+        """
+        form = CommentForm({'body': 'This is a great post'})
+        self.assertTrue(form.is_valid(), msg="Form is invalid with normal text")
+
+    def test_form_invalid_when_empty(self):
+        """
+        Form should be invalid if body is empty.
+        """
+        form = CommentForm({'body': ''})
+        self.assertFalse(form.is_valid(), msg="Form is valid but empty")
+
+    def test_form_valid_with_special_characters(self):
+        """
+        Form should accept special characters and emojis.
+        """
+        special_text = "Wow! 😎🔥 <b>Bold Text</b> & special chars ©®™"
+        form = CommentForm({'body': special_text})
+        self.assertTrue(form.is_valid(), msg="Form is invalid with special characters")
